2f334592ff61d0d53c451830eea9958e0a02dc7f
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-11-24 11:19:48 -0600

    feat: refactor HomeControllerClass.java to HomeController.java and refactor contents to redirect to return home as well as creating templates/home.html to link HomeController.java and home.html

--- src/main/java/com/codeup/adlister/controllers/HomeController.java
+++ src/main/java/com/codeup/adlister/controllers/HomeController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeControllerClass {
-    @GetMapping("/")
-    @ResponseBody
-    public String Message() {
-        return "This is the landing page!";
+public class HomeController {
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
     }
 }
