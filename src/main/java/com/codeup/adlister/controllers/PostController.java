3c991999b37f27e9f3ab26f77e744964ddfe9e3f
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-11-27 18:44:23 -0600

    feat: looked back at the curriculum and it seems like it is not necessary to click on an individual ad and to display that ad information in show.html, followed the instructions and created a new object to be passed in show.html. Would like to learn how to encapsulate a posts data and transfer it to another page.

--- src/main/java/com/codeup/adlister/controllers/PostController.java
+++ src/main/java/com/codeup/adlister/controllers/PostController.java
@@ -2,10 +2,7 @@ package com.codeup.adlister.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,24 +30,35 @@ public class PostController {
 
     @GetMapping("/posts")
     public String GetPosts(Model model) {
+        posts();
+        model.addAttribute("posts", posts());
+        return "/posts/index";
+    }
+
+    @GetMapping("/posts/show")
+    public String GetPost(Model model) {
+        Post post = new Post("1984 Schwinn Bike", "Stored in a storage unit since 1990 and kept covered to help prevent environmental exposure. In very good condition");
+        model.addAttribute("post", post);
+
+        //has to stay this way so there is a path to show.html...
+        return "posts/show";
+    }
+
+    public List<Post> posts() {
         List<Post> postList = new ArrayList<>();
         Post post1 = new Post("Fleece Sweater", "Like new Eddie Bauer fleece sweater, worn once.");
         Post post2 = new Post("Sharpie Marker's 24ct", "Unopened box of large tip sharpie markers");
         Post post3 = new Post("Personal Vaporizer", "VooPoo Drag 2 Platinum for sale. Comes with 3 extra FireLuke Max 10ohm coils");
 
+//        Post post1 = new Post(1,"Fleece Sweater", "Like new Eddie Bauer fleece sweater, worn once.");
+//        Post post2 = new Post(2,"Sharpie Marker's 24ct", "Unopened box of large tip sharpie markers");
+//        Post post3 = new Post(3,"Personal Vaporizer", "VooPoo Drag 2 Platinum for sale. Comes with 3 extra FireLuke Max 10ohm coils");
+
         postList.add(post1);
         postList.add(post2);
         postList.add(post3);
-
-        model.addAttribute("posts", postList);
-        return "/posts/index";
+        return postList;
     }
 
-
-
-//    public List<Post> defaultPosts() {
-//
-//        return postList;
-//    }
 }
 
