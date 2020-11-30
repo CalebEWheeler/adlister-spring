1d66f0e1de0001efbe651224edc8e3b90fee3f52
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-11-30 10:14:48 -0600

    feat: put info into AdController.java to show all ads and remove examples.properties so it will be in .gitignore

--- src/main/java/com/codeup/adlister/controllers/AdController.java
+++ src/main/java/com/codeup/adlister/controllers/AdController.java
@@ -1,4 +1,21 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.controllers.Repository.AdRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdController {
+    private final AdRepository adDao;
+
+    public AdController(AdRepository adDao) {
+        this.adDao = adDao;
+    }
+
+    @GetMapping("/ads")
+    public String index(Model model) {
+        model.addAttribute("ads", adDao.findAll());
+        return "ads/index";
+    }
 }
