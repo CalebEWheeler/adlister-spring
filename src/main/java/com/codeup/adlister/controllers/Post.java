9c278e110ff1ed1cbe33815a336c8b428700c77c
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-11-30 10:28:48 -0600

    feat: add Annotations to Post.java class

--- src/main/java/com/codeup/adlister/controllers/Post.java
+++ src/main/java/com/codeup/adlister/controllers/Post.java
@@ -1,18 +1,24 @@
 package com.codeup.adlister.controllers;
 
+import javax.persistence.*;
+
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable=false, length=100)
     private String title;
+
+    @Column(nullable=false)
     private String body;
-//    private long id;
 
     public Post(String title, String body) {
-//        this.id = id;
         this.title = title;
         this.body = body;
     }
 
-//    public long getId() {return id;}
-//    public void setId() {this.id = id;}
     public String getTitle() {return title;}
     public void setTitle(String title) {this.title = title;}
     public String getBody() {return body;}
