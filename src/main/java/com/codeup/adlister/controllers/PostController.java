a7b71527353987c5cbd7d756931355369b31efc7
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-01 13:36:15 -0600

    feat: add a method UpdatePost() that will take in the id of the current ad from the url and update the current post's title and body fields in the database

--- src/main/java/com/codeup/adlister/controllers/PostController.java
+++ src/main/java/com/codeup/adlister/controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.Repository.PostRepository;
+import com.codeup.adlister.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,6 @@ public class PostController {
 //    public String PostPostPage() {
 //        return "create a new post";
 //    }
-
     @GetMapping("/posts")
     public String GetPosts(Model model) {
         model.addAttribute("posts", postDao.findAll());
@@ -31,8 +31,20 @@ public class PostController {
     public String GetPost(@PathVariable String n, Model model) {
         Long id = Long.parseLong(n);
         model.addAttribute("post", postDao.getOne(id));
-        return "posts/show";
+        return "/posts/show";
+    }
+//    TODO: create a method to edit a post
+
+    @PostMapping("/show/{n}")
+    public String UpdatePost(
+            @PathVariable long n,
+            @RequestParam(name="editTitle") String title,
+            @RequestParam(name="editBody") String body) {
+        Post post = new Post(n, title, body);
+        postDao.save(post);
+        return "redirect:/show/"+post.getId();
     }
 
+//    TODO: create a method to delete a post
 }
 
