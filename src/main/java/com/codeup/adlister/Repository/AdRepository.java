ee6c88b105ab7d7fe0dd3658999c752a5000da9c
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-01 09:09:04 -0600

    feat: add @Repository annotations to Repository files

--- src/main/java/com/codeup/adlister/Repository/AdRepository.java
+++ src/main/java/com/codeup/adlister/Repository/AdRepository.java
@@ -1,7 +1,9 @@
 package com.codeup.adlister.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.codeup.adlister.models.Ad;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdRepository extends JpaRepository<Ad, Long> {
 
 }
--- src/main/java/com/codeup/adlister/Repository/PostRepository.java
+++ src/main/java/com/codeup/adlister/Repository/PostRepository.java
@@ -1,7 +1,9 @@
 package com.codeup.adlister.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.codeup.adlister.models.Post;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
