86722529c983e8272c874078d66c416e0f791899
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-03 10:22:59 -0600

    feat: created getter and setter for posts in User model

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -38,4 +38,6 @@ public class User {
     public void setEmail(String email) {this.email=email;}
     public String getPassword() {return password;}
     public void setPassword(String password) {this.password=password;}
+    public List<Post> getPosts() {return posts;}
+    public void setPosts(List<Post> posts) {this.posts = posts;}
 }
