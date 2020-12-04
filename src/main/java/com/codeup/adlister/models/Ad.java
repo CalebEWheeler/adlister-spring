f03deee35a2f36dadef181c20aab9ab6680774a8
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-04 13:17:03 -0600

    feat: fill out the structure of the ad model by connecting it to a user through the constructor and creating getters and setters for ad properties

--- src/main/java/com/codeup/adlister/models/Ad.java
+++ src/main/java/com/codeup/adlister/models/Ad.java
@@ -13,4 +13,32 @@ public class Ad {
 
     @Column(nullable=false)
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    public Ad() {}
+
+    public Ad(long id, String title, String description, User user) {
+        this.id=id;
+        this.title=title;
+        this.description=description;
+        this.user=user;
+    }
+
+    public Ad(String title, String description, User user) {
+        this.title=title;
+        this.description=description;
+        this.user=user;
+    }
+
+    public long getId() {return id;}
+    public void setId(long id) {this.id=id;}
+    public String getTitle() {return title;}
+    public void setTitle(String title) {this.title=title;}
+    public String getDescription() {return description;}
+    public void setDescription(String description) {this.description=description;}
+    public User getUser() {return user;}
+    public void setUser(User user) {this.user=user;}
 }
