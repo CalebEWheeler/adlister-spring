7644c2347a7ff2866f70b62e2ed8b4eb98c40a80
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-08 16:26:16 -0600

    feat: add partials to home.html, and add files to handle testing

--- src/main/java/com/codeup/adlister/Repository/AdRepository.java
+++ src/main/java/com/codeup/adlister/Repository/AdRepository.java
@@ -3,7 +3,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.codeup.adlister.models.Ad;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AdRepository extends JpaRepository<Ad, Long> {
-
+    Ad findByDescription(String desc); // mysql> select * from ads where description = ?;
+    List<Ad> findAllByTitleIsLike(String term);
+    Ad findByTitle(String title);
 }
